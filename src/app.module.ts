1212c0cbaed0c1e06abbb8971d8f275c00f7be69
maxcorp <shaparyuk@gmail.com>
2019-08-26 20:48:48 +0300

    db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig)
+  ],
 })
 export class AppModule {}
