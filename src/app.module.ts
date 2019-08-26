dd661a9bad198e2597d744fffa0c983a11833d4d
maxcorp <shaparyuk@gmail.com>
2019-08-26 22:43:41 +0300

    added modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot(typeOrmConfig),
+      CategoriesModule,
+  ],
 })
 export class AppModule {}
