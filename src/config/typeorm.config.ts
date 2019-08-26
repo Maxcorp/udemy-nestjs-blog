6f9ba87788586670b6b7726a04c6464dbcec53b1
maxcorp <shaparyuk@gmail.com>
2019-08-26 22:27:19 +0300

    added db config

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: any = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'udemy-blog',
+    entities: [__dirname + '/../**/*entity.ts'],
+    synchronize: true,
+}
