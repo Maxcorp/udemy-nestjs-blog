929060996820a5ee4b2a21dfb76363e89e4786c6
maxcorp <shaparyuk@gmail.com>
2019-08-29 17:17:59 +0300

    added articles controller and service

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,23 +1,23 @@
-import { Controller, Get, Post, Patch, Param, ParseIntPipe, Body, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, ParseIntPipe, Patch, Delete } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
-import { ArticleDto } from 'dist/articles/dto/article.dto';
+import { ArticleDto } from './dto/article.dto';
 
 @Controller('articles')
 export class ArticlesController {
     constructor(private articlesService: ArticlesService) {}
 
     @Get()
-    getCategories() {
+    getArticle() {
         
     }
 
     @Post()
-    createCategory(@Body() articleDto: ArticleDto) {
+    createAricle(@Body() articleDto: ArticleDto) {
         
     }
 
     @Patch('/:id')
-    updateCategory(@Param('id', ParseIntPipe) id: number, articleDto: ArticleDto) {
+    updateArticle(@Param('id', ParseIntPipe) id: number, articleDto: ArticleDto) {
         
     }
 
